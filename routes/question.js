2dc0b122450d5a1e36006a8cc545a0b8afc8d393
linjjzz <1322558575@qq.com>
2024-06-20 16:24:02 +0800

    feat：问卷操作

--- routes/question.js
+++ routes/question.js
@@ -5,6 +5,7 @@ router.prefix('/api/question')
 
 const Random = Mock.Random
 
+// 详情
 router.get('/:id', function (ctx, next) {
   const { id } = ctx.params
   ctx.body = {
@@ -16,19 +17,23 @@ router.get('/:id', function (ctx, next) {
   }
 })
 
+// 列表
 router.get('/', function (ctx, next) {
-  const { url = '' } = ctx
+  const { url = '', query = {} } = ctx
   const isDeleted = url.indexOf('isDeleted=true') >= 0
   const isStar = url.indexOf('isStar=true') >= 0
+  const pageSize = parseInt(query.pageSize) || 10
+
   ctx.body = {
     code: 0,
     data: {
-      list: getQuestionList({ isDeleted, isStar }),
+      list: getQuestionList({ len: pageSize, isDeleted, isStar }),
       total: 100, //总数
     }
   }
 })
 
+// 创建
 router.post('/', function (ctx, next) {
   ctx.body = {
     code: 0,
@@ -38,9 +43,38 @@ router.post('/', function (ctx, next) {
   }
 })
 
+// 修改
+router.patch('/:id', function (ctx, next) {
+  ctx.body = {
+    code: 0,
+  }
+})
+
+// 批量恢复
+router.patch('/', function (ctx, next) {
+  ctx.body = {
+    code: 0,
+  }
+})
+
+// 批量删除
+router.delete('/', function (ctx, next) {
+  ctx.body = {
+    code: 0,
+  }
+})
+
+// 复制
+router.post('/duplicate/:id', function (ctx, next) {
+  const { id } = ctx.params
+  ctx.body = {
+    code: 0,
+    id
+  }
+})
 
 function getQuestionList(opt) {
-  const { len = 10, isDeleted = false, isStar = false } = opt
+  const { len, isDeleted = false, isStar = false } = opt
   const list = []
   for (let i = 0; i < len; i++) {
     list.push({
